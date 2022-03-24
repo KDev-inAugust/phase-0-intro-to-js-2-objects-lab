564fb5640166e6ef34efb3c096f58504f453648a
Kane Mathis <admin@kanemathis.com>
2022-03-24 13:30:30 -0700

    commit tree 168a8fd3ae6fd26456dda93ef773f588a2630080

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name:"test",
+    address:" test address",
+}
+
+console.log(employee);
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+  let newEmployee={...obj}; 
+newEmployee[key]=value;
+return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    let newEmployee2={...obj};
+    delete newEmployee2[key];
+    return newEmployee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
